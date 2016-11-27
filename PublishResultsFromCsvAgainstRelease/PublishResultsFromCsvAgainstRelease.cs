978b5129ab03358d66727eeb4fecc0c51a9e4ff2
Manoj <manoj.bableshwar@gmail.com>
2016-11-27 12:40:04 +0530

    checking out release variables...

---
+++ PublishResultsFromCsvAgainstRelease/PublishResultsFromCsvAgainstRelease.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+// https://www.nuget.org/packages/Microsoft.TeamFoundationServer.Client/
+using Microsoft.TeamFoundation.TestManagement.WebApi;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.InteractiveClient/
+using Microsoft.VisualStudio.Services.Client;
+
+// https://www.nuget.org/packages/Microsoft.VisualStudio.Services.Client/
+using Microsoft.VisualStudio.Services.Common;
+
+namespace PublishResultsFromCsvAgainstRelease
+{
+    class PublishResultsFromCsvAgainstRelease
+    {
+        static void Main(string[] args)
+        {
+
+            foreach (System.Collections.DictionaryEntry env in Environment.GetEnvironmentVariables())
+            {
+                string name = (string)env.Key;
+                string value = (string)env.Value;
+                Console.WriteLine("{0}={1}", name, value);
+            }
+        }
+    }
+}
